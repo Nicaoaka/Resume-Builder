50271b61598807637ebda64f6fa69fbcc69180ec
brandon <157628053+shigeosapsycho@users.noreply.github.com>
2024-10-27 10:23:50 -0700

    Update flask resume builder.py

--- flask resume builder.py
+++ flask resume builder.py
@@ -16,7 +16,7 @@ app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
 # Initialize Inference Client
-API_KEY = os.getenv("HF_API_KEY", "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")
+API_KEY = os.getenv("HF_API_KEY", "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") # THIS KEY IS KILLED AFTER THE HACKATHON PLEASE GENERATE YOUR OWN!
 client = InferenceClient(api_key=API_KEY)
 
 # Helper Functions
